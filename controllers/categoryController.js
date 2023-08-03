4686f00fced6d79e295b77da2d694d7cd73cd1aa
unbearabletruth <antaarty@gmail.com>
2023-08-03 16:56:51 +0700

    create category

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -12,9 +12,53 @@ exports.index = asyncHandler(async (req, res, next) => {
 });
 
 exports.items_list = asyncHandler(async (req, res, next) => {
-    const items = await Item.find({ category: req.params.id }).exec()
+    const [category, items] = await Promise.all([
+      Category.findById(req.params.id).exec(),
+      Item.find({ category: req.params.id }).exec()
+    ]);
     res.render("items_list", {
-      title: "All CPUs",
+      title: category.name,
       items: items
     });
 });
+
+exports.category_create_get = (req, res, next) => {
+  res.render("category_create", { title: "Create category" });
+};
+
+exports.category_create_post = [
+  body("name", "Category name must contain at least 3 characters")
+    .trim()
+    .isLength({ min: 3 })
+    .escape(),
+  body("description", "Category description must contain at least 8 characters")
+    .trim()
+    .isLength({ min: 8 })
+    .escape(),
+
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req);
+
+    const category = new Category({
+      name: req.body.name,
+      description: req.body.description
+    });
+
+    if (!errors.isEmpty()) {
+      res.render("category_create", {
+        title: "Create category",
+        category: category,
+        errors: errors.array(),
+      });
+      return;
+    } else {
+      const categoryExists = await Category.findOne({ name: req.body.name }).exec();
+      if (categoryExists) {
+        res.redirect(categoryExists.url);
+      } else {
+        await category.save();
+        res.redirect(category.url);
+      }
+    }
+  }),
+];
--- routes/categories.js
+++ routes/categories.js
@@ -7,6 +7,10 @@ const category_controller = require("../controllers/categoryController");
 
 router.get("/", category_controller.index);
 
+router.get("/category/create", category_controller.category_create_get);
+
+router.post("/category/create", category_controller.category_create_post);
+
 router.get('/category/:id', category_controller.items_list)
 
 router.use('/category/:id', itemRouter)
