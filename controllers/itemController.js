79e3926a0b7c5886b92a4eccff93138716ffc14f
unbearabletruth <antaarty@gmail.com>
2023-08-04 13:09:04 +0700

    create item

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -1,11 +1,74 @@
 const Item = require("../models/item");
+const Category = require("../models/category");
 const asyncHandler = require("express-async-handler");
 const { body, validationResult } = require("express-validator");
 
 exports.item_details = asyncHandler(async (req, res, next) => {
-    console.log(req.params)
     const item = await Item.findById(req.params.itemId).exec()
     res.render("item_details", {
       item: item
     });
 });
+
+exports.item_create_get = asyncHandler(async (req, res, next) => {
+  const categories = await Category.find().exec()
+  res.render("item_form", { 
+    title: "Create item",
+    categories: categories,
+    current_category: req.params.id
+  });
+});
+
+exports.item_create_post = [
+  body("name", "Category name must contain at least 3 characters")
+    .trim()
+    .isLength({ min: 3 })
+    .escape(),
+  body("description", "Category description must contain at least 8 characters")
+    .trim()
+    .isLength({ min: 8 })
+    .escape(),
+  body("category", "Choose category.")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body("price", "Item must have a price.")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body("number_in_stock", "Item must have a price.")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req);
+
+    const item = new Item({
+      name: req.body.name,
+      description: req.body.description,
+      category: req.body.category,
+      price: req.body.price,
+      number_in_stock: req.body.number_in_stock
+    });
+
+    if (!errors.isEmpty()) {
+      const categories = await Category.find().exec()
+      res.render("item_form", {
+        title: "Create item",
+        categories: categories,
+        item: item,
+        errors: errors.array(),
+      });
+      return;
+    } else {
+      const itemExists = await Item.findOne({ name: req.body.name }).exec();
+      if (itemExists) {
+        res.redirect(itemExists.url);
+      } else {
+        await item.save();
+        res.redirect(item.url);
+      }
+    }
+  }),
+];
--- routes/items.js
+++ routes/items.js
@@ -3,6 +3,18 @@ const router = express.Router({ mergeParams: true });
 
 const item_controller = require("../controllers/itemController");
 
+router.get('/item/create', item_controller.item_create_get);
+
+router.post('/item/create', item_controller.item_create_post);
+
+//router.get("/item/:itemId/delete", item_controller.item_delete_get);
+
+//router.post("/item/:itemId/delete", item_controller.item_delete_post);
+
+//router.get("/item/:itemId/update", item_controller.item_update_get);
+
+//router.post("/item/:itemId/update", item_controller.item_update_post);
+
 router.get('/item/:itemId', item_controller.item_details)
 
 module.exports = router;
